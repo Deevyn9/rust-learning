2b3ab479d186db01f2db5e7d0f651984d471df93
Divine Obeten <divineobeten9@gmail.com>
2025-04-27 21:14:13 +0100

    rust learning: hello world, variables, data types

---
+++ basics/data_types/src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    // integers
+    // let x: i8 = 10;
+    // println!("{}", x);
+
+    // let _y: u8 = 10;
+
+    // let decimal = 02_55;
+    // let hex = 0xff;
+    // let octal = 0o377;
+    // let binary = 0b1111_1111;
+
+    // println! {"{}", decimal};
+    // println! {"{}", hex};
+    // println! {"{}", octal};
+    // println! {"{}", binary};
+
+    // let byte = b'A';
+    // println! {"{}", byte};
+
+    // floating point
+    // let x = 2.0; // f64 default
+    // let y: f32 = 1.0;
+
+    // booleans
+    // let t = true;
+    // let f: bool = false;
+
+    // character type
+    // let c = "c";
+
+    // + - * / % - arithmetics
+
+    // let a = 10;
+    // let b = 4;
+
+    // let remainder = a % b;
+    // println!("{}", remainder);
+
+    // tuples
+    // let tup = (500, "hi", true);
+    // println!("{}", tup.0);
+    // let (x, y, z) = tup;
+
+    // println!("{}", x);
+    // println!("{}", y);
+    // println!("{}", z);
+
+    // arrays
+    let array = [1, 2, 3];
+
+    println!("{}", array[0]);
+
+    let mut array2: [i32; 3] = [4, 5, 6];
+    println!("{}", array2[0]);
+    array2[0] = 10;
+    println!("{}", array2[2]);
+}
